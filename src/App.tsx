0c42ef42f696babbc1f0d5e3d8e3bee6caff027f
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-07-21 00:10:30 -0300

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { defaultTheme } from "./styles/themes/default";
 import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
 import GlobalStyle from "./styles/global";
+import { router } from "./router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      Hello World
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
