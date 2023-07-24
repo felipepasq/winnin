261fe7e466f8756d4938f3ae606ed0a9a73de766
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-07-24 03:41:01 -0300

    finished first test

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,9 +1,12 @@
 import { render } from "../../../utils/test-utils";
+import { screen } from "@testing-library/react";
 import Button from "..";
 
+const TEXT_MOCK = "TEXT_MOCK";
+
 describe("Test Button", () => {
-  it("teste render", () => {
-    render(<Button>Teste</Button>);
-    expect(1).toBe(1);
+  it("should render", () => {
+    render(<Button>{TEXT_MOCK}</Button>);
+    expect(screen.getByText(TEXT_MOCK)).toBeDefined();
   });
 });
